ebdfe888e591475e881dcd428c2111f9505bc1bc
Gustavo-Aquino <81302289+Gustavo-Aquino@users.noreply.github.com>
2021-03-25 09:54:21 -0300

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+
+#define NTHREADS  2         //total de threads a serem criadas
+#define VectorSize 1000     //tamanho do vetor
+
+int vector[VectorSize];     //variavel global para o vetor
+
+//estrutura basica para guardar os argumentos da funcao 
+typedef struct {
+   int ini, end;
+} t_Args;
+
+//funcao executada pelas threads
+void *ChangeVector (void *arg) {
+    t_Args *args = (t_Args *) arg;
+    int i=0;
+
+    //simples acrescimo nos valores da funcao
+    for(i=args->ini;i<args->end;i++) vector[i]++;
+
+    free(arg); //aqui pode liberar a alocacao feita na main
+    pthread_exit(NULL);
+}
+
+//funcao teste para checar o vetor
+void Teste (){
+    for(int i=0;i<VectorSize;i++){
+        if(vector[i]!=1){
+            printf("Erro na mudanca\n"); return;
+        } 
+    }
+    printf("Vetor mudado com sucesso\n");
+}
+
+//funcao principal do programa
+int main() {
+    pthread_t tid_sistema[NTHREADS];//identificadores das threads no sistema
+    int thread;                     //variavel auxiliar
+    t_Args *arg;                    //receberá os argumentos para a thread
+
+    for(thread=0; thread<NTHREADS; thread++) {
+        arg = malloc(sizeof(t_Args));
+        if (arg == NULL) {
+            printf("--ERRO: malloc()\n"); exit(-1);
+        }
+
+        //Preenche a estrutura com as informacoes necessarias
+        arg->ini = thread*VectorSize/2; 
+        arg->end = arg->ini+VectorSize/2; 
+        
+        //Cria a thread e realiza sua funcao
+        if (pthread_create(&tid_sistema[thread], NULL, ChangeVector, (void*) arg)) {
+            printf("--ERRO: pthread_create()\n"); exit(-1);
+        }
+    }
+
+    //espera todas as threads terminarem
+    //garantindo que todo o vetor seja alterado
+    for (thread=0; thread<NTHREADS; thread++) {
+        if (pthread_join(tid_sistema[thread], NULL)) {
+            printf("--ERRO: pthread_join() \n"); exit(-1); 
+        } 
+    }
+
+    //Testa
+    Teste();
+    
+    pthread_exit(NULL);
+}
